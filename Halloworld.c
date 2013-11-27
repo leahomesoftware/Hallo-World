61dacb3b984f983388f048d770338d4c05643ce5
leadel ngalame <lng@yahoo.com>
2013-11-27 10:15:48 +0100

    Committed changed made by develop

--- Halloworld.c
+++ Halloworld.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
  int main()
  {
+   int x,y,z;
+   x= 10;
+   y= 20;
+   z= 30;
+
    printf("This is a Halloworld project\n");
+   printf("This is a change i made on the file\n");
+
+  // print out all the values of the variables declared above
+
+   printf("The value of x = %d \n " x);
+   printf("The value of y = %d \n", y);
+   printf("The value of z = %d \n", z);
+
+//getchar() will cause the screen to wait for a moment and till the user press in some button
    getchar();
    return 0;
  
